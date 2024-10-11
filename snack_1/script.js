6591f19251bc7d2c4839d77b1f7fa43ba68aa010
AlessioMura <alessiomura03@gmail.com>
2024-10-11 14:49:59 +0200

    initial commit
    added snack1, added bicycle array

---
+++ snack_1/script.js
@@ -0,0 +1,29 @@
+//Snack 1
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
+
+const biciDaCorsa = [
+    {
+        nome: 'bici_1',
+        peso: 7.2,
+    },
+    {
+        nome: 'bici_2',
+        peso: 6.9,
+    },
+    {
+        nome: 'bici_3',
+        peso: 6.8,
+    },
+    {
+        nome: 'bici_4',
+        peso: 7.1,
+    },
+    {
+        nome: 'bici_5',
+        peso: 6.7,
+    }
+];
+
+console.log(biciDaCorsa);
