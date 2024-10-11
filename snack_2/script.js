d5bece126dfb7ba38500123dd6340b5b6b75b66c
AlessioMura <alessiomura03@gmail.com>
2024-10-11 15:34:46 +0200

    added random numbers in requested properties

--- snack_2/script.js
+++ snack_2/script.js
@@ -2,7 +2,7 @@
 //Creare un array di oggetti di squadre di calcio.
 //Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-const squadre = [
+const teams = [
     {
         nome: 'Juventus',
         punti_fatti: 0,
@@ -26,5 +26,18 @@ const squadre = [
 ];
 
 //Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
+
+function rndm_nmbrs(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+for (let i = 0; i < teams.length; i++) {
+    teams[i].punti_fatti = rndm_nmbrs(1, 30);
+    teams[i].falli_subiti = rndm_nmbrs(1, 30);
+
+
+}
+console.log(teams);
 
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
